f82600209e181b72c3473f559a090007e861c881
Erick Lugo <ericklugoj@gmail.com>
2022-05-19 01:35:00 -0500

    Added cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
   constructor() {
@@ -15,6 +16,7 @@ class Server {
 
   runMiddlewares() {
     this.app.use(express.static(path.resolve(__dirname, '../public')));
+    this.app.use(cors());
   }
 
   execute() {
